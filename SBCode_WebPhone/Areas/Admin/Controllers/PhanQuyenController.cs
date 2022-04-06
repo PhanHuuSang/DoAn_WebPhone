677ecfbf5f2e9b60cd95dae384e51c293d8774e7
SANGPHAN\SANGPHAN <sangkcr2000@gmail.com>
2022-04-06 20:16:33 +0700

    100%

---
+++ SBCode_WebPhone/Areas/Admin/Controllers/PhanQuyenController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SBCode_WebPhone.Data;
+using SBCode_WebPhone.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SBCode_WebPhone.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class PhanQuyenController : Controller
+    {
+        private readonly MyDBContext _dbcontext;
+
+        public PhanQuyenController(MyDBContext db)
+        {
+            _dbcontext = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult PhanQuyen()
+        {
+
+            var data = _dbcontext.KhachHangs.Include(kh => kh.UserRoles)
+                .Select(kh => new PhanQuyenVM
+                {
+                    MaKh = kh.MaKh,
+                    HoTen = kh.HoTen,
+                    QuanTri = KiemtraPQ(kh, 1),
+                    BanHang = KiemtraPQ(kh, 2),
+                    ThuKho = KiemtraPQ(kh, 3),
+                    KhachHang = KiemtraPQ(kh, 4),
+                });
+            return View(data);
+        }
+
+
+
+        private static bool KiemtraPQ(object phanQuyen, int a)
+        {
+            var check = ((KhachHang)phanQuyen).UserRoles.FirstOrDefault(x => x.RoleId == a);
+            if (check != null)
+                return true;
+            else
+                return false;
+        }
+
+
+        [HttpPost]
+        public IActionResult UpdatePhanQuyen(List<int> MaKh, List<bool> QuanTri, List<bool> ThuKho, List<bool> BanHang, List<bool> KhachHang)
+        {
+            for (int i = 0; i < MaKh.Count; i++)
+            {
+                Save(MaKh[i],QuanTri[i],1);
+                Save(MaKh[i], BanHang[i], 2);
+                Save(MaKh[i], ThuKho[i], 3);
+                Save(MaKh[i], KhachHang[i], 4);
+            }
+            return RedirectToAction("PhanQuyen");
+        }
+
+        public void Save(int MaKh, bool check, int chucvu)
+        {
+            UserRole phanquyen = _dbcontext.UserRoles.FirstOrDefault(p => p.UserId == MaKh && p.RoleId == chucvu);
+             if(check == true && phanquyen == null)
+            {
+                UserRole phanquyen1 = new UserRole()
+                {
+                    UserId = MaKh,
+                    RoleId = chucvu
+                };
+                _dbcontext.Add(phanquyen1);
+                _dbcontext.SaveChanges();
+            }
+            else if (check == false && phanquyen != null)
+            {
+                phanquyen.UserId = MaKh;
+                phanquyen.RoleId = chucvu;
+                _dbcontext.Remove(phanquyen);
+                _dbcontext.SaveChanges();
+            }
+        }
+    }
+}
---
+++ SBCode_WebPhone/Migrations/20210707110207_SeedData.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using SBCode_WebPhone.Helper;
+
+namespace SBCode_WebPhone.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+
+
+            migrationBuilder.InsertData(
+                table: "Role",
+                columns: new[] { "RoleName", "IsSystem" },
+                values: new object[] { "Quản Trị", true });
+            migrationBuilder.InsertData(
+                table: "Role",
+                columns: new[] { "RoleName", "IsSystem" },
+                values: new object[] { "Bán Hàng", true });
+            migrationBuilder.InsertData(
+                table: "Role",
+                columns: new[] { "RoleName", "IsSystem" },
+                values: new object[] { "Thủ Kho", true });
+            migrationBuilder.InsertData(
+                table: "Role",
+                columns: new[] { "RoleName", "IsSystem" },
+                values: new object[] { "Khách Hàng", true });
+
+            migrationBuilder.InsertData(
+                table: "KhachHang",
+                columns: new[] { "MaKh", "DangHoatDong", "DiaChi", "Email", "HoTen", "MaNgauNhien", "MatKhau", "SoDienThoai" },
+                values: new object[] { 1, true, "GiaLai", "admin@gmail.com", "Admin", "01", "admin".ToSHA512Hash("01"), "0868811469" });
+
+            migrationBuilder.InsertData(
+               table: "UserRole",
+               columns: new[] { "RoleId", "UserId" },
+               values: new object[] { 1, 1});
+
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            //migrationBuilder.DeleteData(
+            //    table: "KhachHang",
+            //    keyColumn: "MaKh",
+            //    keyValue: 1);
+        }
+    }
+}
